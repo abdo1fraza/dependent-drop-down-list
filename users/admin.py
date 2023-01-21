6ac245ea35f8f448b6f4c7a1e9bd96c213fcb835
abdo1fraza <abdo87.hamza@gmail.com>
2023-01-21 20:00:01 +0200

    initial project

---
+++ users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from users.models import College, Branch, Student
+
+admin.site.register(College)
+admin.site.register(Branch)
+admin.site.register(Student)
+
+
---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class College(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return str(self.name)
+
+class Branch(models.Model):
+    college = models.ForeignKey(College, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 60)
+
+    def __str__(self):
+        return str(self.name)
+
+class Student(models.Model):
+    name = models.CharField(max_length = 35)
+    birthdate = models.DateTimeField(null = True , blank = True )    
+    college = models.ForeignKey(College, on_delete = models.SET_NULL, null=True)
+    branch = models.ForeignKey(Branch, on_delete = models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
